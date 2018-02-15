6bdcc3fd8c469686505063b5d8945c003475eb32
Shane Osbourne <shane.osbourne8@gmail.com>
2018-02-15 15:40:57 +0000

    Add Build Module

---
+++ app/code/Jh/Build/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    map: {
+        "*": {
+            'jquery/jquery.mobile.custom': 'Jh_Build/js/jquery.mobile.custom',
+            'Magento_Ui/js/form/element/region': 'Jh_Build/js/region',
+            'Magento_PageCache/js/page-cache': 'Jh_Build/js/page-cache'
+        }
+    },
+};
